05b117c3b6bba1e37000a1dee77634e02491cd0b
grizz-bot92 <bbenoit752@gmail.com>
2025-02-02 09:40:23 -0800

    grouped elements into sprites

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ def main():
 	pygame.init()
 	clock = pygame.time.Clock()
 	dt = 0
-	player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
@@ -16,15 +15,29 @@ def main():
 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+	player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+
+
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 
 		screen.fill("black")
-		player.draw(screen)
+		updatable.update(dt)
+
+		for sprite in drawable:
+			sprite.draw(screen)
+
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
 	main()
