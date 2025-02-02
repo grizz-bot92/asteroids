d748356a05fffddc75c89ff1d5966ab1b21a78aa
grizz-bot92 <bbenoit752@gmail.com>
2025-02-01 21:20:20 -0800

    added player class

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from constants import *
+from players import *
 import pygame
 
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
+	player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
@@ -18,9 +22,9 @@ def main():
 				return
 
 		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
-
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
-
