3bb008c895537397cf40a8426e3731b9fd7edf34
grizz-bot92 <bbenoit752@gmail.com>
2025-02-01 16:45:53 -0800

    updated loops

--- main.py
+++ main.py
@@ -3,25 +3,22 @@ import pygame
 
 def main():
 	pygame.init()
-	
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	screen_on = True
-
-	while screen_on:
-		screen.fill("black")
-		pygame.display.flip()
-
-
-
-
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			return
-
 
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+
+		screen.fill("black")
+		pygame.display.flip()
 
 
 if __name__ == "__main__":
